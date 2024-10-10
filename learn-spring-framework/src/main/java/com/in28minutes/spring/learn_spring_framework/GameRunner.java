f9efe68d256477e6166add73da81baaffc527980
Alvarez Cerrillo <Mijail.AlvarezCerrillo@nordstrom.com>
2024-10-10 14:01:01 -0600

    first lesson

--- learn-spring-framework/src/main/java/com/in28minutes/spring/learn_spring_framework/GameRunner.java
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learn_spring_framework/GameRunner.java
@@ -1,7 +1,16 @@
 package com.in28minutes.spring.learn_spring_framework;
 
 public class GameRunner {
-    private MarioGame
+    private MarioGame game;
+
     public GameRunner(MarioGame game) {
+        this.game = game;
+    }
+
+    public void run() {
+        game.up();
+        game.down();
+        game.left();
+        game.right();
     }
 }
--- learn-spring-framework/src/main/java/com/in28minutes/spring/learn_spring_framework/MarioGame.java
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learn_spring_framework/MarioGame.java
@@ -1,4 +1,19 @@
 package com.in28minutes.spring.learn_spring_framework;
 
 public class MarioGame {
+    public void up() {
+        System.out.println("jump");
+    }
+
+    public void down() {
+        System.out.println("down into a hole");
+    }
+
+    public void left() {
+        System.out.println("stop");
+    }
+
+    public void right() {
+        System.out.println("accelerate");
+    }
 }
